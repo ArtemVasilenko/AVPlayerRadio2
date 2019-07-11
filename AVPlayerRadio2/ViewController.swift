fb0389a25b6d27248face09258025a96f6553d0a
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-11 19:48:25 +0300

    +++

--- AVPlayerRadio2/ViewController.swift
+++ AVPlayerRadio2/ViewController.swift
@@ -1,16 +1,30 @@
 import UIKit
-import AVFoundation
-//import AVKit
+//import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
     
     
     let rabbitPath = "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
     
+    let myButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 30, y: 30, width: 40, height: 40)
+        button.setTitle("GO!", for: .normal)
+        button.titleLabel?.textColor = .red
+        button.backgroundColor = .green
+        
+        button.addTarget(self, action: #selector(getController), for: .touchDown)
+        
+        return button
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getRabbitInNora()
+        self.view.addSubview(myButton)
+//        getRabbitInNora()
+        getController()
     }
     
     
@@ -23,6 +37,24 @@ class ViewController: UIViewController {
         layer.frame = self.view.bounds
         self.view.layer.addSublayer(layer)
         player.play()
+        
+    }
+    
+    @objc func getController() {
+        guard let urlWatch = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8") else { return }
+        
+        let player = AVPlayer(url: urlWatch)
+        
+//        let playerLayer = AVPlayerLayer(player: player)
+//        playerLayer.frame = self.view.bounds
+//        self.view.layer.addSublayer(playerLayer)
+        
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        present(controller, animated: true) {
+            player.play()
+        }
         
     }
     
